bd96b32eef91bb8e8252befa446116f93f1e13a6
Alberto <oalberto96c@gmail.com>
2017-11-22 15:46:21 -0800

    Add JSON format to http request

--- app/src/main/java/com/computomovilyubicuo/audiosender/MainActivity.java
+++ app/src/main/java/com/computomovilyubicuo/audiosender/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -13,11 +15,14 @@ import com.computomovilyubicuo.audiosender.utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView et_temperature;
+    TextView et_humidity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_temperature = (EditText) findViewById(R.id.et_temperature);
+        et_humidity = (EditText) findViewById(R.id.et_humidity);
     }
 
     protected void onBtnClick_sendfile(View v){
@@ -26,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void sendFile(){
+        String humidity = et_humidity.getText().toString();
+        String temperature = et_temperature.getText().toString();
+
         RequestQueue queue = Volley.newRequestQueue(this);
         NetworkUtils network_utils = new NetworkUtils();
-        StringRequest str_request = network_utils.getRequest();
+        StringRequest str_request = network_utils.getRequest(humidity,temperature);
         queue.add(str_request);
     }
 }
--- app/src/main/java/com/computomovilyubicuo/audiosender/utilities/NetworkUtils.java
+++ app/src/main/java/com/computomovilyubicuo/audiosender/utilities/NetworkUtils.java
@@ -1,6 +1,7 @@
 package com.computomovilyubicuo.audiosender.utilities;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.computomovilyubicuo.audiosender.MainActivity;
 
 import com.android.volley.Request;
@@ -11,7 +12,11 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.computomovilyubicuo.audiosender.MainActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -24,23 +29,54 @@ import java.net.UnknownHostException;
 
 public class NetworkUtils {
 
-    public static final String HOST = "http://10.0.0.2:8000";
+    public static final String HOST = "http://192.168.0.117/arduinodata";
 
-    public StringRequest getRequest(){
+    public String getBodyRequest(String humidity, String temperature) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("humidity", Float.valueOf(humidity));
+        json.put("temperature", Float.valueOf(temperature));
+        return json.toString();
+    }
+
+    public StringRequest getRequest(final String humidity, final String temperature){
 
 // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, HOST,
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HOST,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
+                        Log.d("Response ", response);
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.wtf("NetworkUtils: ","Error on response");
+                        Log.wtf("NetworkUtils: ",error.toString());
                     }
-        });
+
+        }){
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    String body = getBodyRequest(humidity, temperature);
+                    Log.d("String:", body);
+
+                    return body == null ? null : body.getBytes("utf-8");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return null;
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        };
         return stringRequest;
     }
 
